174eb1d270cc1dcb95c1fb0305b78c35a67ac1d8
Erelae <s.enriquez@outlook.fr>
2026-06-29 22:08:38 +0200

    update translation files organization + en json files + add local storage (to save user language choice)

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 function changeLang(langue) {
-  fetch(langue + ".json").then(function (response) {
+  localStorage.setItem("lang", langue);
+
+  const page = document.body.dataset.page || 'index';
+  fetch("lang/" + langue + "/" + page + ".json").then(function (response) {
     response.json().then(function (data) {
       Object.keys(data).forEach((cle) => {
-        document.querySelector("#" + cle).innerHTML = data[cle];
+        const el = document.querySelector("#" + cle);
+        if (el) el.innerHTML = data[cle];
       });
+      document.documentElement.lang = langue;
     });
   });
 
   document.querySelectorAll(".lang-btn").forEach((btn) => {
     btn.classList.remove("active");
+    btn.setAttribute("aria-pressed", "false");
   });
 
   document.querySelector("#btn-" + langue).classList.add("active");
+  document.querySelector("#btn-" + langue).setAttribute("aria-pressed", "true");
+}
+
+const savedLang = localStorage.getItem("lang");
+if (savedLang && savedLang !== "fr") {
+  changeLang(savedLang);
 }
